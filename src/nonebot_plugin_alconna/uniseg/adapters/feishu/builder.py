b506d0f2024d11526e2f5ec3f5cfcc5b6eeb77c2
wyf7685 <wyf7685@163.com>
2025-04-26 18:16:47 +0800

    ✨ feishu: flatten post and parse reply

--- src/nonebot_plugin_alconna/uniseg/adapters/feishu/builder.py
+++ src/nonebot_plugin_alconna/uniseg/adapters/feishu/builder.py
@@ -8,11 +8,14 @@ from nonebot.adapters.feishu.message import Post as PostSegment
 from nonebot.adapters.feishu.message import Audio as AudioSegment
 from nonebot.adapters.feishu.message import Image as ImageSegment
 from nonebot.adapters.feishu.message import Media as MediaSegment
+from nonebot.adapters.feishu.models import UserId as FeishuUserId
 from nonebot.adapters.feishu.message import Folder as FolderSegment
+from nonebot.adapters.feishu.message import Message, MessageSegment
+from nonebot.adapters.feishu.models import Mention as FeishuMention
 
 from nonebot_plugin_alconna.uniseg.constraint import SupportAdapter
 from nonebot_plugin_alconna.uniseg.builder import MessageBuilder, build
-from nonebot_plugin_alconna.uniseg.segment import At, File, AtAll, Audio, Hyper, Image, Reply, Video
+from nonebot_plugin_alconna.uniseg.segment import At, File, Text, AtAll, Audio, Emoji, Image, Reply, Video
 
 
 class FeishuMessageBuilder(MessageBuilder):
@@ -26,13 +29,13 @@ class FeishuMessageBuilder(MessageBuilder):
             return AtAll(here=seg.data["user_id"] == "here")
         return At("user", str(seg.data["user_id"]))
 
-    @build("image")
+    @build("image", "img")  # img for PostImg
     def image(self, seg: ImageSegment):
         return Image(id=seg.data["image_key"])
 
     @build("media")
     def media(self, seg: MediaSegment):
-        return Video(id=seg.data["file_key"], name=seg.data["file_name"] or "video.mp4")
+        return Video(id=seg.data["file_key"], name=seg.data.get("file_name") or "video.mp4")
 
     @build("audio")
     def audio(self, seg: AudioSegment):
@@ -52,13 +55,45 @@ class FeishuMessageBuilder(MessageBuilder):
             name=seg.data.get("file_name") or seg.data["file_key"],
         )
 
+    @build("a")  # PostA
+    def a(self, seg: MessageSegment):
+        return Text(text=seg.data["text"]).link()
+
+    @build("emotion")  # PostEmotion
+    def emotion(self, seg: MessageSegment):
+        return Emoji(id=seg.data["emoji_type"])
+
     @build("post")
     def post(self, seg: PostSegment):
-        return Hyper("json", content=dict(seg.data))
+        result = []
+        # ref: https://github.com/nonebot/adapter-feishu/blob/v2.6.2/nonebot/adapters/feishu/message.py#L428
+        for line in seg.data["content"]:
+            for node in line:  # should be PostMessageNode
+                node = node.copy()
+                converted = self.convert(MessageSegment(node.pop("tag"), node))
+                result.extend(converted if isinstance(converted, list) else [converted])
+        return result
 
     async def extract_reply(self, event: Event, bot: Bot):
         if TYPE_CHECKING:
             assert isinstance(event, MessageEvent)
         if event.reply:
-            return Reply(event.reply.message_id, event.reply.body.content, event.reply)
+            # ReplyMention is incompatible with Mention
+            # but Message.deserialize only uses Mention.id.open_id
+            # ref: https://github.com/nonebot/adapter-feishu/blob/v2.6.2/nonebot/adapters/feishu/message.py#L642
+            mentions = [
+                FeishuMention(
+                    key=m.key,
+                    id=FeishuUserId(union_id="", user_id="", open_id=(m.id if m.id_type == "open_id" else "")),
+                    name=m.name,
+                    tenant_key=m.tenant_key,
+                )
+                for m in event.reply.mentions
+            ]
+            msg = Message.deserialize(
+                content=event.reply.body.content,
+                mentions=mentions,
+                message_type=event.reply.msg_type,
+            )
+            return Reply(event.reply.message_id, msg, event.reply)
         return None
